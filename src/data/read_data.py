3d2231866cf5c80498413bdd27a0f19f2d67480c
avinax <avinax@gmail.com>
2021-12-19 00:42:14 +0530

    read raw data

---
+++ src/data/read_data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+
+def get_raw_data():
+    raw_data = pd.read_csv('C:/Users/Avinash/classification/data/raw/kc_house_data.csv')
+    return raw_data
+
